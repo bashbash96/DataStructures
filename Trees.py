ca09c7d6b7c1ad337ccfda3dbe4a94744e767ef6
bashbash <amjad.bashiti@hotmail.com>
2020-03-10 16:55:28 +0200

    add Trie implementation wtih Hash Map

--- Trees.py
+++ Trees.py
@@ -90,6 +90,56 @@ class Queue:
         if curr is not None:
             print(curr.data)
 
+# ------------------- Trie implementation with hashMap ------------------
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        curr = self.root
+        for c in word:
+            if c not in curr:
+                curr[c] = {}
+            curr = curr[c]
+        curr['*'] = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        curr = self.root
+
+        for c in word:
+            if c not in curr:
+                return False
+            curr = curr[c]
+
+        if '*' in curr:
+            return True
+
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        curr = self.root
+
+        for c in prefix:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        else:
+            return True
+
 
 # ------------------- Binary Search Tree implementation with Linked List ------------------
 class BST:
